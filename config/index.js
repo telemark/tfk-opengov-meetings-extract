ae21586f0824d724af06a3e3d1e179b8d815ded3
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-08 12:20:09 +0200

    Initial commit

--- config/index.js
+++ config/index.js
@@ -6,6 +6,7 @@ module.exports = {
   host: envs.OPENGOV_HOST || 'http://opengov.cloudapp.net',
   path: envs.OPENGOV_PATH || '/Meetings/tfk',
   year: envs.OPENGOV_YEAR || new Date().getFullYear(),
+  fileDirectory: envs.OPENGOV_FILE_DIRECTORY || 'meetings',
   activeBoards: envs.OPENGOV_ACTIVE_BOARDS || [
     '216162'
   ]
