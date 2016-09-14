a007d763e57d034e2fc332771dae6aa8440c6302
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-09-14 12:40:36 +0200

    Updates config. Updates opengov-meetings

--- config/index.js
+++ config/index.js
@@ -8,6 +8,6 @@ module.exports = {
   year: envs.OPENGOV_YEAR || new Date().getFullYear(),
   fileDirectory: envs.OPENGOV_FILE_DIRECTORY || 'meetings',
   activeBoards: envs.OPENGOV_ACTIVE_BOARDS ? envs.OPENGOV_ACTIVE_BOARDS.split(',') : [
-    '216162'
+    '216162', '216167', '217261', '217335', '217336', '217337', '217338', '217339', '217340', '217341', '217342', '217344', '217345', '218688', '220291', '222101', '225118'
   ]
 }
